156f18253013b01f2f0c15fb156fb390f68acf20
Kayky Nogueira <114272397+Kaykynog@users.noreply.github.com>
2023-01-16 11:16:28 -0300

    16-01-2023

---
+++ empacotamento_kargs.py
@@ -0,0 +1,10 @@
+# Empacotamento e desempacotamento de dicionários
+a, b = 1, 2
+a, b = b, a
+
+pessoa = {
+    'nome': 'Kayky',
+    'sobrenome': 'Nogueira',
+}
+a , b = pessoa.values()
+print(a, b)
